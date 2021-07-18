68a38342aed7b3e81c322928093ef8d97d687112
livmari <liv.alterskjaer@icloud.com>
2021-07-18 19:11:34 +0200

    Server base set up with apollo and express

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+async function startApolloServer() {
+  // Construct a schema, using GraphQL schema language
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `
+
+  // Provide resolver functions for your schema fields
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  }
+
+  const server = new ApolloServer({ typeDefs, resolvers })
+  await server.start()
+
+  const app = express()
+  server.applyMiddleware({ app })
+
+  await app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  )
+
+  return { server, app }
+}
+
+startApolloServer()
