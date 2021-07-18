1a626be046330d8358cac85240e9c8dbe37c0a03
livmari <liv.alterskjaer@icloud.com>
2021-07-18 21:44:23 +0200

    User entity created, only id and email right now

---
+++ src/entities/user.ts
@@ -0,0 +1,15 @@
+import { prop as Property, getModelForClass } from '@typegoose/typegoose'
+import { ObjectId } from 'mongodb'
+import { Field, ObjectType } from 'type-graphql'
+
+@ObjectType()
+export class User {
+  @Field()
+  readonly _id!: ObjectId
+
+  @Field()
+  @Property({ required: true })
+  email!: string
+}
+
+export const UserModel = getModelForClass(User)
